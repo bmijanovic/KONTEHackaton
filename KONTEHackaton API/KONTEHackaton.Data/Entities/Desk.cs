a4c445d0d596ba4f27adbbb8932e136849db9dc1
Bojan Mijanovic <mijanovic.boki.bojan@gmail.com>
2022-03-08 00:04:24 +0100

    added data files

---
+++ KONTEHackaton API/KONTEHackaton.Data/Entities/Desk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace KONTEHackaton.Data.Entities
+{
+    [Table("Desk")]
+    public class Desk
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("order")]
+        public int Order { get; set; }
+
+        [Column("avaiable")]
+        public int avaiable { get; set; }
+
+        public Room Room { get; set; }
+
+        public Guid RoomId { get; set; }
+    }
+}
---
+++ KONTEHackaton API/KONTEHackaton.Data/Entities/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace KONTEHackaton.Data.Entities
+{
+    [Table("Room")]
+    public class Room
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Desk> Desks { get; set; }
+
+        public Faculty Faculty { get; set; }
+
+        [Column("facultyId")]
+        public Guid FacultyId { get; set; }
+
+    }
+}
+
