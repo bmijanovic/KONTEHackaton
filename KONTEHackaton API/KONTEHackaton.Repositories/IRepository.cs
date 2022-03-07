120d1f6594847e7298ad0104d5ca09da98cad1a3
Bojan Mijanovic <mijanovic.boki.bojan@gmail.com>
2022-03-08 00:47:23 +0100

    added repositorium layer

---
+++ KONTEHackaton API/KONTEHackaton.Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KONTEHackaton.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetById(Guid id);
+        Task<IEnumerable<T>> GetAll();
+
+    }
+}
---
+++ KONTEHackaton API/KONTEHackaton.Repositories/RoomsInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KONTEHackaton.Data.Context;
+using KONTEHackaton.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KONTEHackaton.Repositories
+{
+    public interface IRoomsRepository : IRepository<Room>
+    {
+
+    }
+
+    public class RoomsRepository : IRoomsRepository
+    {
+        private FacultyContext _facultyContext;
+
+        public RoomsRepository(FacultyContext facultyRepository)
+        {
+            _facultyContext = facultyRepository;
+        }
+
+        public async Task<IEnumerable<Room>> GetAll()
+        {
+            return await _facultyContext.Rooms.ToListAsync();
+        }
+
+        public async Task<Room> GetById(Guid id)
+        {
+            var data = await _facultyContext.Rooms.FindAsync(id);
+            return data;
+        }
+
+   
+    }
+}
